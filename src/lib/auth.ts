8ded9ea19a2d6a566ded6e73e64bda2a4cd29f31
@dev.arthur <contato.arthurreis222@gmail.com>
2024-12-20 13:27:31 -0300

    chore: add setup prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,11 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Nodemailer from "next-auth/providers/nodemailer";
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "./prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(db),
   providers: [
     Nodemailer({
       server: {
